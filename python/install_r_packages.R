232ce7a2a31b79c8da38c3fd560a3bcd07bae490
AlexVogt <alexvogt101@gmail.com>
2024-08-21 12:04:01 +0200

    attempt at fixing batch mode error

--- python/install_r_packages.R
+++ python/install_r_packages.R
@@ -1,4 +1,4 @@
 # Installs the required R-packages
 install.packages("remotes", repos = "https://cloud.r-project.org")
-remotes::install_github("NorskRegnesentral/shapr")
+remotes::install_github("AlexVogt1/shapr/.git")
 # Installs the development version of shapr from the master branch on CRAN
